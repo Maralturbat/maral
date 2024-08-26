df6264b2428faa4e6b8c50ee7b72342b1995ed08
Maralturbat <maralturbat0630@gmail.com>
2024-08-26 20:55:46 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let number = 12;
+const string = "maral";
+const boolean = 3 < 2;
+const object = {
+    name: "maral",
+    age: 14,
+    nan: true,
+};
+object.cool = true;
+console.log(object.cool);
+const array = [5, 2, 3];
+const array1 = ["meat", "egg", "milk"];
+const array2 = [true, false];
+const array3 = [
+    { name: "maral", age: 14 },
+    { name: "manlaitur", age: 3 },
+];
+const sayHello = () => {
+    console.log("Henloo");
+};
+sayHello("marla");
+sayHello("manlaitur");
+const add = (a, b) => {
+    return a + b; // 1 + 2
+};
+const sum = (1, 2);
